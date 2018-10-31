b80669845a03aea1fd4cee810e0fdd887dde9721
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2018-10-31 13:29:36 +0100

    chore(eslint): update react version to 16

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         "react": {
             "createClass": "createReactClass",
             "pragma": "React",
-            "version": "15.0"
+            "version": "16"
         },
     },
     "rules": {
